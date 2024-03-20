4d4b68f280e630295ede6ea110800a58d6fedde4
FabriG <fabriciogatti7@gmail.com>
2024-03-20 10:14:06 -0300

    Organized code

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,12 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 
-const { Client } = require('@notionhq/client');
-const notion = new Client({ auth: process.env.NOTION_KEY });
+const { getDatabase } = require('./controllers/database.controller');
 
 app.use(express.static("public"))
 app.use(express.json()) // for parsing application/json
 
-app.post("/databases", async (req, res) => {
-    const databaseId = process.env.NOTION_DATABASE_ID;
-
-    try {
-        const response = await notion.databases.query({
-            database_id: databaseId,
-        });
-        res.json(response);
-    } catch (error) {
-        res.json({ error: error.message });
-    }
-});
+app.post("/databases", getDatabase);
 
 const listener = app.listen(process.env.PORT, function () {
     console.log("Your app is listening on port " + listener.address().port)
